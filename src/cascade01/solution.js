a518028533dd94929eff396dcf4b75d208c17564
Shahar Talmi <shahar@wix.com>
2025-01-26 19:00:05 +0200

    solving part 3

--- src/cascade01/solution.js
+++ src/cascade01/solution.js
@@ -1,4 +1,4 @@
-export function solve(input) {
+export function solve1(input) {
   let chars = input.split("");
   let result = 0;
   for (let i = 1; i <= chars.length / 2; i++) {
@@ -7,7 +7,7 @@ export function solve(input) {
   return result;
 }
 
-export function solve2(input, times = 1000) {
+export function solve2(input, slice = 1, times = 1000) {
   let parts = [
     [parseInt(input.slice(0, 5), 16)],
     [parseInt(input.slice(5, 10), 16)],
@@ -17,6 +17,37 @@ export function solve2(input, times = 1000) {
     parts.forEach(part => part.push((part.at(-1) * 421 + 37) % 3872873));
   }
   return parts
-    .flatMap(part => part.slice(-4).map(x => (x % 16).toString(16)))
+    .flatMap(part => part.slice(slice))
+    .map(x => (x % 16).toString(16))
     .join("");
+}
+
+export function solve3(input, energy = 1024) {
+  if (input.length === 32) input = solve2(input).slice(0, 1000);
+  input = input.split("");
+  let roots = [0, 1, 2, 3, 4]
+    .map(i => ({ digits: input.slice(i * 200, (i + 1) * 200), energy }))
+    .filter(root => root.digits.length > 0);
+  let result = 0;
+  for (let i = 0; i * 5 < 200; i++) {
+    let drops = 0;
+    let add = [];
+    for (let root of roots) {
+      let next = root.digits.slice(i * 5, (i + 1) * 5).join("");
+      next = parseInt(next, 16).toString(2);
+      // let distance = parseInt(next.slice(0, 7), 2);
+      let drop = Math.min(root.energy, 5 + parseInt(next.slice(7, 10), 2));
+      // let direction = parseInt(next.slice(10), 2);
+      root.energy -= drop;
+      drops += drop;
+      if (root.energy >= 4) {
+        let give = Math.floor(root.energy / 4);
+        add.push({ ...root, energy: give });
+        root.energy -= give;
+      }
+    }
+    result += (i + 1) * drops;
+    roots.push(...add);
+  }
+  return result;
 }
--- src/cascade01/solution.spec.js
+++ src/cascade01/solution.spec.js
@@ -1,26 +1,46 @@
-import { solve, solve2 } from "./solution.js";
+import { solve1, solve2, solve3 } from "./solution.js";
 import readInputs from "../utils/read-inputs.js";
 
 let inputs = readInputs(import.meta.url);
 
 describe("cascade01 task01", () => {
   test("it should work for examples", () => {
-    expect(solve("01ab2f")).toEqual(84);
+    expect(solve1("01ab2f")).toEqual(84);
   });
 
   test("it should work for input", () => {
-    expect(solve(...inputs)).toEqual(1935);
+    expect(solve1(...inputs)).toEqual(1935);
   });
 });
 
 describe("cascade01 task02", () => {
   test("it should work for examples", () => {
-    expect(solve2("123456789abcdef00000000000000000", 4)).toEqual(
+    expect(solve2("123456789abcdef00000000000000000", -4, 4)).toEqual(
       "67a498a2c910",
     );
   });
 
   test("it should work for input", () => {
-    expect(solve2(...inputs)).toEqual("170a239e18e1");
+    expect(solve2(...inputs, -4)).toEqual("170a239e18e1");
+  });
+});
+
+describe("cascade01 task03", () => {
+  test("it should work for examples", () => {
+    expect(
+      solve3(
+        new Array(5).fill("aa155aa555aa955aad55ab155ab555ab955abd55").join(""),
+        20,
+      ),
+    ).toEqual(41);
+    expect(
+      solve3(
+        new Array(5).fill("aa155aa555aa955aad55ab155ab555ab955abd55").join(""),
+      ),
+    ).toEqual(7209);
+  });
+
+  test("it should work for input", () => {
+    expect(solve3(...inputs)).toEqual(36111);
   });
 });
